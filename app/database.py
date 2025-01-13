5f1e207d5cbe5fa350701c5d1bc9187bcf504ed5
rahuldeopa <rahuldeopa1818@gmail.com>
2025-01-13 14:59:37 +0530

    updated

---
+++ app/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine, Column, Integer, Float, ForeignKey, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import datetime
+
+DATABASE_URL = "postgresql://user:password@db:5432/auction_db"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class Bid(Base):
+    __tablename__ = 'bids'
+    id = Column(Integer, primary_key=True, index=True)
+    auction_id = Column(Integer)
+    user_id = Column(Integer)
+    amount = Column(Float)
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+
+Base.metadata.create_all(bind=engine)
