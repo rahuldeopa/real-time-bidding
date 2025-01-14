880f79000f0516e3da735a6dc3148d5079270b56
rahuldeopa <rahuldeopa1818@gmail.com>
2025-01-14 08:33:57 +0530

    file

--- app/database.py
+++ app/database.py
@@ -1,9 +1,9 @@
-from sqlalchemy import create_engine, Column, Integer, Float, ForeignKey, DateTime
+from sqlalchemy import create_engine, Column, Integer, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
 
-DATABASE_URL = "postgresql://user:password@db:5432/auction_db"
+DATABASE_URL = "mysql+pymysql://user:password@db:3306/auction_db"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
